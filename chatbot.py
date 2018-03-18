0dfb39a95138e7307522c7f7fc5106cd749eb60b
Dan <okeeffed090@gmail.com>
2018-03-18 19:31:32 -0400

    commit 18/03/2018 a

--- chatbot.py
+++ chatbot.py
@@ -19,3 +19,196 @@ import data
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 
+def _get_random_bucket(train_bucket_scale):
+    """
+    Get a random bucket from which to choose a training sample
+    :param train_bucket_scale:
+    :return:
+    """
+    rand = random.random()
+    return min([i for i in range(len(train_bucket_scale)) if train_bucket_scale[i] > rand])
+
+
+def _assert_lengths(encoder_size, decoder_size, encoder_inputs, decoder_inputs, decoder_masks):
+    """
+    Assert that the encoder inputs, decoder inputs, and decoder masks are of the expected lengths
+    :param encoder_size:
+    :param decoder_size:
+    :param encoder_inputs:
+    :param decoder_inputs:
+    :param decoder_masks:
+    :return:
+    """
+    if len(encoder_inputs) != encoder_size:
+        raise ValueError("Encoder length must be equal to the one in bucket," 
+                         "%d != %d." % (len(encoder_inputs), encoder_size))
+    if len(decoder_inputs) != decoder_size:
+        raise ValueError("Decoder length must be equal to the one in bucket," 
+                         "%d != %d." % (len(decoder_inputs), decoder_size))
+    if len(decoder_masks) != decoder_size:
+        raise ValueError("Weights length must be equal to the one in bucket," 
+                         "%d != %d." % (len(decoder_masks), decoder_size))
+
+
+def run_step(sess, model, encoder_inputs, decoder_inputs, decoder_masks, bucket_id, forward_only):
+    """
+    Run one step in training.
+    @forward_only: boolean value to decide whether a backward path should be created.
+    forward_only is set to True when you just want to evaluate on the test, set, or when you want to run the bot in chat
+    mode.
+    :param sess:
+    :param model:
+    :param encoder_inputs:
+    :param decoder_inputs:
+    :param decoder_masks:
+    :param bucket_id:
+    :param forward_only:
+    :return:
+    """
+    encoder_size, decoder_size = config.BUCKETS[bucket_id]
+    _assert_lengths(encoder_size, decoder_size, encoder_inputs, decoder_inputs, decoder_masks)
+
+    # input feed: encoder inputs, decoder inputs, target_weights, as provided.
+    input_feed = {}
+    for step in range(encoder_size):
+        input_feed[model.encoder_inputs[step].name] = encoder_inputs[step]
+    for step in range(decoder_size):
+        input_feed[model.decoder_inputs[step].name] = decoder_inputs[step]
+        input_feed[model.decoder_masks[step].name] = decoder_masks[step]
+
+    last_target = model.decoder_inputs[decoder_size].name
+    input_feed[last_target] = np.zeros([model.batch_size], dtype=np.int32)
+
+    # output feed: depends on whether we do a backwards step or not.
+    if not forward_only:
+        output_feed = [model.train_ops[bucket_id],
+                       model.gradient_norms[bucket_id],
+                       model.losses[bucket_id]]
+    else:
+        output_feed = [model.losses[bucket_id]]
+        for step in range(decoder_size):
+            output_feed.append(model.outputs[bucket_id][step])
+
+    outputs = sess.run(output_feed, input_feed)
+    if not forward_only:
+        return outputs[1], outputs[2], None # gradient norm, loss, no outputs.
+    else:
+        return None, outputs[0], outputs[1:] # no gradient norm, loss, outputs.
+
+
+def _get_buckets():
+    """
+    Load the dataset into buckets based on their lengths. train_bucket_scale is the interval that'll help us choose a
+    random bucket later on.
+    :return:
+    """
+    test_buckets = data.load_data('test_ids.enc', 'test_ids.dec')
+    data_buckets = data.load_data('train_ids.enc', 'train_ids.dec')
+    train_bucket_sizes = [len(data_buckets[b]) for b in range(len(config.BUCKETS))]
+    print("Number of samples n each bucket:\n", train_bucket_sizes)
+    train_total_size = sum(train_bucket_sizes)
+    train_buckets_scale = [sum(train_bucket_sizes[:i + 1]) / train_total_size for i in range(len(train_bucket_sizes))]
+    print('Bucket scale:\n', train_buckets_scale)
+    return test_buckets, data_buckets, train_buckets_scale
+
+
+def _get_skip_step(iteration):
+    """
+    How many steps should the model train before it saves all the weights.
+    :param iteration:
+    :return:
+    """
+    if iteration < 100:
+        return 30
+    return 100
+
+
+def _check_restore_parameters(sess, saver):
+    """
+    Restore the previously trained parameters if there are any.
+    :param sess:
+    :param saver:
+    :return:
+    """
+    ckpt = tf.train.get_checkpoint_state(os.path.dirname(config.CPT_PATH + '/checkpoint'))
+    if ckpt and ckpt.model_checkpoint_path:
+        print("Loading parameters for the ChatBot.")
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        print("Initializing fresh parameters for the ChatBot.")
+
+
+def _eval_test_set(sess, model, test_buckets):
+    """
+    Evaluate on test set.
+    :param sess:
+    :param model:
+    :param test_buckets:
+    :return:
+    """
+    for bucket_id in range(len(config.BUCKETS)):
+        if len(test_buckets[bucket_id]) == 0:
+            print("  Test: empty bucket %d" % (bucket_id))
+            continue
+        start = time.time()
+        encoder_inputs, decoder_inputs, decoder_masks = data.get_batch(test_buckets[bucket_id],
+                                                                       bucket_id,
+                                                                       batch_size=config.BATCH_SIZE)
+        _, step_loss, _ = run_step(sess, model, encoder_inputs, decoder_inputs, decoder_masks, bucket_id, True)
+        print("Test bucket {0}: loss {1}, time {2}".format(bucket_id, step_loss, time.time() - start))
+
+
+def train():
+    """
+    Train the bot.
+    :return:
+    """
+    pass
+
+
+def _get_user_input():
+    """
+    Get user's input, which will be transformed into encoder input later.
+    :return:
+    """
+    print(">", end="")
+    sys.stdout.flush()
+    return sys.stdin.readline()
+
+
+def find_right_bucket(length):
+    """
+    Find the proper bucket for an encoder input based on its length
+    :param length:
+    :return:
+    """
+    return min([b for b in range(len(config.BUCKETS)) if config.BUCKETS[b][0] >= length])
+
+
+def _construct_reponse(output_logits, inv_dec_vocab):
+    """
+    Construct a reponse to the user's encoder input.
+    @output_logits: the outputs from sequence to sequence to sequence wrapper.
+    output_logits is decoder_size np array, each of dim 1 x DEC_VOCAB
+    This is a greedy decoder - outputs are just argmaxes of output_logits.
+    :param output_logits:
+    :param inv_dec_vocab:
+    :return:
+    """
+    pass
+
+
+def chat():
+    """
+    in test mode, we don't create the backward path
+    :return:
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- data.py
+++ data.py
@@ -61,5 +61,227 @@ def question_answers(id2line, convos):
     return questions, answers
 
 
+def make_dir(path):
+    """
+    create a directory is there isn't one already
+    :param path:
+    :return:
+    """
+    try:
+        os.mkdir(path)
+    except OSError:
+        pass
+
+
 def prepare_dataset(questions, answers):
-    pass
+    # create path to store all the train and test encoders and decoders
+    make_dir(config.PROCESSED_PATH)
+
+    # select random conversations to make the test set.
+    test_ids = random.sample([i for i in range(len(questions))], config.TESTSET_SIZE)
+
+    filenames = ['train.enc', 'train.dec', 'test.enc', 'train.enc']
+    files = []
+    for filename in filenames:
+        files.appened(open(os.path.join(config.PROCESSED_PATH, filename), 'w'))
+
+    for i in range(len(questions)):
+        if i in test_ids:
+            files[2].write(questions[i] + '\n')
+            files[3].write(answers[i] + '\n')
+        else:
+            files[0].write(questions[i] + '\n')
+            files[1].write(answers[i] + '\n')
+
+    for file_ in files:
+        file_.close()
+
+
+def basic_tokenizer(line, normalize_digits=True):
+    """
+    A basic tokenizer.  I can probably replace this with nltk!
+    :param line:
+    :param normalize_digits:
+    :return:
+    """
+    line = re.sub('<u>', '', line)
+    line = re.sub('</u>', '', line)
+    line = re.sub('\[', '', line)
+    line = re.sub('/]', '', line)
+    words = []
+    _WORD_SPLIT  = re.compile("([.,!?\"'-<>:;)(])")
+    _DIGIT_RE = re.compile(r"\d")
+    for fragment in line.strip().lower().split():
+        for token in re.split(_WORD_SPLIT, fragment):
+            if not token:
+                continue
+            if normalize_digits:
+                token = re.sub(_DIGIT_RE, '#', token)
+            words.append(token)
+    return words
+
+
+def build_vocab(filename, normalize_digits=True):
+    in_path = os.path.join(config.PROCESSED_PATH, filename)
+    out_path = os.path.join(config.PROCESSED_PATH, 'vocab.{0}'.format(filename[-3:]))
+
+    vocab = {}
+    with open(in_path, 'r') as f:
+        for line in f.readlines():
+            for token in basic_tokenizer(line):
+                if not token in vocab:
+                    vocab[token] = 0
+                vocab[token] += 1
+
+    sorted_vocab = sorted(vocab, key=vocab.get, reverse=True)
+    with open(out_path, 'w') as f:
+        f.write('<pad>' + '\n')
+        f.write('<unk>' + '\n')
+        f.write('<s>' + '\n')
+        f.write('<\s>' + '\n')
+        index = 4
+        for word in sorted_vocab:
+            if vocab[word] < config.THRESHOLD:
+                break
+            f.write(word + '\n')
+            index += 1
+        with open('config.py', 'a') as cf:
+            if filename[-3:] == 'enc':
+                cf.write('ENC_VOCAB = ' + str(index) + '\n')
+            else:
+                cf.write('DEC_VOCAB = ' + str(index) + '\n')
+
+
+def load_vocab(vocab_path):
+    with open(vocab_path, 'r') as f:
+        words = f.read().splitlines()
+    return words, {words[i]: i for i in range(len(words))}
+
+
+def sentence2id(vocab, line):
+    return [vocab.get(token, vocab['<unk>']) for token in basic_tokenizer(line)]
+
+
+def token2id(data, mode):
+    """
+    Convert all tokens in the daet into their corresponding index in the vocabulary.
+    :param data:
+    :param mode:
+    :return:
+    """
+    vocab_path = 'vocab.' + mode
+    in_path = data + '.' + mode
+    out_path = data + '_ids.' + mode
+
+    _, vocab = load_vocab(os.path.join(config.PROCESSED_PATH, vocab_path))
+    in_file = open(os.path.join(config.PROCESSED_PATH, in_path), 'r')
+    out_file = open(os.path.join(config.PROCESSED_PATH, out_path), 'w')
+
+    lines = in_file.read().splitlines()
+    for line in lines:
+        if mode == 'dec': # we only care about '<s>' and </s> in encoder
+            ids = [vocab['<s>']]
+        else:
+            ids = []
+        ids.extend(sentence2id(vocab, line))
+        if mode == 'dec':
+            ids.append(vocab['</s>'])
+        out_file.write(' '.join(str(id_) for id_ in ids) + '\n')
+
+
+def prepare_raw_data():
+    print('Preparing raw data into training and testing sets ...')
+    id2line = get_lines()
+    convos = get_convos()
+    questions, answers = question_answers(id2line, convos)
+    prepare_dataset(questions, answers)
+
+
+def process_data():
+    print('Preparing data to be model ready ...')
+    build_vocab('train.enc')
+    build_vocab('train.dec')
+    token2id('train', 'enc')
+    token2id('train', 'dec')
+    token2id('test', 'enc')
+    token2id('test', 'dec')
+
+
+def load_data(enc_filename, dec_filename, max_training_size=None):
+    encode_file = open(os.path.join(config.PROCESSED_PATH, enc_filename), 'r')
+    decode_file = open(os.path.join(config.PROCESSED_PATH, dec_filename), 'r')
+    encode, decode = encode_file.readlines(), decode_file.readline()
+    data_buckets = [[] for _ in config.BUCKETS]
+    i = 0
+    while encode and decode:
+        if (i + 1) % 10000 == 0:
+            print('Bucketing conversation number', i)
+        encode_ids = [int(id_) for id_ in encode.split()]
+        decode_ids = [int(id_) for id_ in decode.split()]
+        for bucket_id, (encode_max_size, decode_max_size) in enumerate(config.BUCKETS):
+            if len(encode_ids) <= encode_max_size and len(decode_ids) <= decode_max_size:
+                data_buckets[bucket_id].append([encode_ids, decode_ids])
+                break
+        encode, decode = encode_file.readline(), decode_file.readline()
+        i += 1
+    return data_buckets
+
+
+def _pad_input(input_, size):
+    return input_ + [config.PAD_ID] * (size - len(input_))
+
+
+def _reshape_batch(inputs, size, batch_size):
+    """
+    Create batch-major inputs. Batch inputs are just re-indexed inputs
+    :param inputs:
+    :param size:
+    :param batch_size:
+    :return:
+    """
+    batch_inputs = []
+    for length_id in range(size):
+        batch_inputs.append(np.array([inputs[batch_id][length_id] for batch_id in range(batch_size)], dtype=np.int32))
+
+    return batch_inputs
+
+
+def get_batch(data_bucket, bucket_id, batch_size=1):
+    """
+    Return one batch to feed into the model
+    :param data_bucket:
+    :param bucket_id:
+    :param batch_size:
+    :return:
+    """
+    encoder_size, decoder_size = config.BUCKETS[bucket_id]
+    encoder_inputs, decoder_inputs = [], []
+
+    for _ in range(batch_size):
+        encoder_input, decoder_input = random.choice(data_bucket)
+        # pad both encoder and decoder, reverse the encoder
+        encoder_inputs.append(list(reversed(_pad_input(encoder_input, encoder_size))))
+        decoder_inputs.append(_pad_input(decoder_input, decoder_size))
+
+    # Create batch-major vectors from the data selected above.
+    batch_encoder_inputs = _reshape_batch(encoder_inputs, encoder_size, batch_size)
+    batch_decoder_inputs = _reshape_batch(decoder_inputs, decoder_size, batch_size)
+
+    # create decoder_masks to be 0 for decoders that are padding.
+    batch_masks = []
+    for length_id in range(decoder_size):
+        batch_mask = np.ones(batch_size, dtype=np.float32)
+        for batch_id in range(batch_size):
+            # Set the mask to 0 is the corresponding target in a PAD symbol.
+            # the corresponding decoder is decoder_input shifted by 1 forward.
+            if length_id < decoder_size - 1:
+                target = decoder_inputs[batch_id][length_id + 1]
+            if length_id == decoder_size - 1 or target == config.PAD_ID:
+                batch_mask[batch_id] = 0.0
+        batch_masks.append(batch_mask)
+    return batch_encoder_inputs, batch_decoder_inputs, batch_masks
+
+
+if __name__ == '__main__':
+    prepare_raw_data()
+    process_data()
